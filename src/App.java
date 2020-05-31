5bb51b7ac6af5b8d9c9092b8ade12be6f5fb6589
brunobrand <bruno8117@gmail.com>
2020-05-31 03:34:57 -0300

    inicio do main

--- src/App.java
+++ src/App.java
@@ -4,23 +4,45 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class App {
     public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
         ListaStopwords ls = new ListaStopwords();
         ManipulaStopWords.guardaStopWords(ls);
-
         LinkedListPalavra lp = new LinkedListPalavra();
         ManipulaTextos.leLivro(lp, ls);
-
-        System.out.println(lp);
-
-        // Criar lista na classe Palavra
-
-        //Mudei o método contains da LinkedListPalavra
-
-
+        
+        System.out.println("Bem-vindo(a) ao nosso trabalho de ALEST I; ");
+        int opcao;
+        do{
+        System.out.println("Digite 0 para sair do programa: ");
+        System.out.println("Digite 1 para exibir todo o índice remissivo: ");
+        System.out.println("Digite 2 para exibir o porcentual de stopwords do texto: ");
+        System.out.println("Digite 3 para encontrar a palavra mais frequente do texto: ");
+        System.out.println("Digite 4 para pesquisar uma palavra específica ");
+        opcao = in.nextInt();
+        switch(opcao){
+            case 1: System.out.println(lp.toString()); break;
+            case 2: break;
+            case 3: break;
+            case 4: pesquisaPalavra(); break;
+            default: System.out.println("Fim do programa. "); break;
+        }
+        }while(opcao!=0);
 
 
     }
+
+    public static void pesquisaPalavra(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite a palavra que deseja pesquisar: ");
+        String umaPalavra = in.nextLine();
+        //chama o método que pesquisa a palavra e printa
+        System.out.println("Digite a pagina que deseja visualizar: ");
+        int umaPagina = in.nextInt();
+        //chama o método que printa a pagina desejada
+    }
+
 }
